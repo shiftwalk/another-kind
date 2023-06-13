960aaac7edc9b3a2ee59d82528e18ff673c1de54
samuelgoddard <hello@samgoddard.co.uk>
2023-06-13 13:02:25 +0100

    mob menu

--- pages/menu.js
+++ pages/menu.js
@@ -36,7 +36,7 @@ export default function Menu() {
                   </nav>
                 </div>
 
-                <div className="w-full relative z-10 translate-y-[-5.5vw] lg:translate-y-0">
+                <div className="w-full relative z-10 translate-y-[-6.75vw] lg:translate-y-0">
                   <Container>
                     <ul className="font-display text-[12vw] leading-none md:text-6xl md:leading-none">
                       <li className="w-full block">
