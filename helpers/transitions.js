40da1fe94306f7be2f3f624cc01e03382301ba09
samuelgoddard <hello@samgoddard.co.uk>
2023-07-03 12:04:12 +0100

    transitions

--- helpers/transitions.js
+++ helpers/transitions.js
@@ -2,7 +2,7 @@ export const fade = {
 	initial: { opacity: 0 },
   enter: { 
     opacity: 1,
-    transition: { duration: 0.6, ease: [0.83, 0, 0.17, 1] }
+    transition: { delay: 0.5, duration: 0.6, ease: [0.83, 0, 0.17, 1] }
   },
 	exit: {
     opacity: 0,
@@ -14,7 +14,7 @@ export const reveal = {
 	initial: { y: '100%' },
   enter: { 
     y: 0,
-    transition: { duration: 0.6, ease: [0.83, 0, 0.17, 1] }
+    transition: { delay: 0.4, duration: 0.6, ease: [0.83, 0, 0.17, 1] }
   },
 	exit: {
     y: '100%',
@@ -48,7 +48,7 @@ export const imageScale = {
 	initial: { scale: 1.075 },
   enter: { 
     scale: 1,
-    transition: { duration: 0.6, ease: [0.83, 0, 0.17, 1] }
+    transition: { delay: 0.5, duration: 0.6, ease: [0.83, 0, 0.17, 1] }
   },
 	exit: {
     scale: 1.075,
