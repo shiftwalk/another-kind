8b7f25d8a6a9577c11eb9409a8a5410b0af8d9e0
samuelgoddard <hello@samgoddard.co.uk>
2023-07-17 13:34:38 +0100

    go live

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   images: {
 		domains: ['cdn.sanity.io'],
-		deviceSizes: [768, 1024, 1280, 1600, 1920, 2400],
-		imageSizes: [32, 64, 96, 128, 256, 350],
+		deviceSizes: [768, 1024, 1280, 1600, 1920, 2400, 3800],
+		imageSizes: [32, 64, 96, 128, 256, 350, 500],
 	},
   webpack(config) {
     config.module.rules.push({
